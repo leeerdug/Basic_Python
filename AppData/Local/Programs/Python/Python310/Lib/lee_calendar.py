44ed48679a929e6b20cf30bf0b9d6ca6889ba6af
Seunghwan Lee <101933474+leeerdug@users.noreply.github.com>
2022-03-20 16:23:57 +0900

    Update lee_calendar.py

--- AppData/Local/Programs/Python/Python310/Lib/lee_calendar.py
+++ AppData/Local/Programs/Python/Python310/Lib/lee_calendar.py
@@ -13,21 +13,21 @@ print("{}년 {}월 {}일 {}시 {}분 {}초".format(
 
 if 3 <= now.month <= 5:
     print("현재 계절은 {}월로 봄입니다!".format(now.month))
-if 6 <= now.month <= 8:
+elif 6 <= now.month <= 8:
     print("현재 계절은 {}월로 여름입니다!".format(now.month))
-if 9 <= now.month <= 11:
+elif 9 <= now.month <= 11:
     print("현재 계절은 {}월로 가을입니다!".format(now.month))
-if 1 <= now.month <= 2 or now.month == 12:
+else:
     print("현재 계절은 {}월로 겨울입니다!".format(now.month))
 
 if 1 <= now.day <= 10:
     print("현재 날짜는 {}일로 {}월 초입니다!".format(now.day,now.month))
-if 11 <= now.day <= 20:
+elif 11 <= now.day <= 20:
     print("현재 날짜는 {}일로 {}월 중순입니다!".format(now.day,now.month))
-if 21 <= now.day <= 31:
+else:
     print("현재 날짜는 {}일로 {}월 말입니다!".format(now.day,now.month))
 
 if now.hour < 12:
     print("현재 시각은 {}시로 오전입니다!".format(now.hour))
-if now.hour >= 12:
+else:
     print("현재 시각은 {}시로 오후입니다!".format(now.hour))
